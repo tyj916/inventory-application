6adecc8ed09fe62ac1c129b507dccd1f3b94cc48
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-11 19:42:14 +0800

    Link controller to database

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,7 +1,11 @@
-function getAllCategories(req, res) {
+const db = require('../db/queries');
+
+async function getAllCategories(req, res) {
+  const categories = await db.getAllCategories();
   res.render('allCategories', {
     title: 'All Categories',
     links: res.locals.links,
+    categories: categories,
   });
 }
 
@@ -12,7 +16,14 @@ function addNewCategoryGet(req, res) {
   });
 }
 
+async function addNewCategoryPost(req, res) {
+  const { name } = req.body;
+  await db.insertCategory(name);
+  res.redirect('/category');
+}
+
 module.exports = {
   getAllCategories,
   addNewCategoryGet,
+  addNewCategoryPost,
 };
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -4,5 +4,6 @@ const categoriesRouter = Router();
 
 categoriesRouter.get('/', categoriesController.getAllCategories);
 categoriesRouter.get('/new', categoriesController.addNewCategoryGet);
+categoriesRouter.post('/new', categoriesController.addNewCategoryPost);
 
 module.exports = categoriesRouter;

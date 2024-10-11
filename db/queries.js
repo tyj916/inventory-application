1fd68c38cdc1de595fb6be43cd560e96bfc8c397
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-11 19:41:33 +0800

    Create category queries

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,15 @@
 const pool = require('./pool');
 
-module.exports = {
+async function getAllCategories() {
+  const { rows } = await pool.query('SELECT * FROM category');
+  return rows;
+}
 
+async function insertCategory(categoryName) {
+  await pool.query(`INSERT INTO category (name) VALUES ($1)`, [categoryName]);
+}
+
+module.exports = {
+  getAllCategories,
+  insertCategory,
 };

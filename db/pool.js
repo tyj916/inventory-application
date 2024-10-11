b2cbb88383df68b3fddaae460dde57733a16d7a9
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-11 17:49:54 +0800

    Set up database query pool

---
+++ db/pool.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+module.exports = new Pool({
+  connectionString: `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:5432/${PGDATABASE}`,
+});
---
+++ db/queries.js
@@ -0,0 +1,5 @@
+const pool = require('./pool');
+
+module.exports = {
+
+};

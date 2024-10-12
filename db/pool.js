f200bb6f0b09197ae599083d750d0d50cc3f9160
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-12 23:40:10 +0800

    Replace connection string with connection uri env

--- db/pool.js
+++ db/pool.js
@@ -4,5 +4,5 @@ const { Pool } = require('pg');
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 module.exports = new Pool({
-  connectionString: `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:5432/${PGDATABASE}?sslmode=require`,
+  connectionString: process.env.CONNECTION_URI,
 });
